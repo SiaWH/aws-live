b1a86befb4c656883cd87982fed4c42fa3d6282c
SiaWH <149123655+SiaWH@users.noreply.github.com>
2024-04-16 16:02:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cr0eoq6ssq1w.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs2573"
+customdb = "employee"
+custombucket = "siaweihang-employee"
+customregion = "us-east-1"
+
